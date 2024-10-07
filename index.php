13e5cb9f7e506981c23fd10ddd9de57e74ee8105
MDorizon <mdorizon@proton.me>
2024-10-07 16:00:31 +0200

    :sparkles: order constructor

--- index.php
+++ index.php
@@ -61,14 +61,29 @@ $vendor_machine_1 = new VendorMachine();
 $vendor_machine_1->shootWithFoot();
 
 class Order {
+    private int $id;
     private array $products;
     private float $totalPrice;
-    private int $id;
-    private string $shippingMethod;
     private string $customerName;
     private datetime $createdAt;
     private string $status;
+    private ?string $shippingMethod;
+    private ?string $shippingAddress;
+    
+    public function __construct(string $customerName, array $products) {
+        $this->status = "CART";
+        $this->createdAt = new DateTime();
+        $this->id = rand();
+
+        $this->products = $products;
+        $this->customerName = $customerName;
+        $this->totalPrice = count($products) * 5;
+
+        echo "Commande {$this->id} créée !";
+    }
 
     
 }
+
+$order = new Order('Jean Pierre', ['Casque', 'Téléphone'])
 ?>
