5b9e7976a45a7f924517f2bcffaecee64977bec0
MDorizon <mdorizon@proton.me>
2024-10-08 10:25:40 +0200

    :sparkles: add new product || listProducts & countTotalPrice methods added

--- Order.php
+++ Order.php
@@ -7,7 +7,9 @@ class Order {
     private datetime $createdAt;
     private string $status;
     private ?string $shippingMethod;
+    private ?string $shippingCity;
     private ?string $shippingAddress;
+    private ?string $shippingCountry;
     
     public function __construct(string $customerName, array $products) {
         $this->status = "CART";
@@ -23,7 +25,7 @@ class Order {
         }
         $this->customerName = $customerName;
         
-        $this->totalPrice = count($products) * 5;
+        $this->countTotalPrice();
         
         echo "Commande {$this->id} créée !";
     }
@@ -33,9 +35,30 @@ class Order {
             unset($this->products[$key]);
         }
 
-        $productAsString = implode(',', $this->products);
+        $this->listProducts();
+    }
 
+    public function addProduct(string $product): void {
+        if (in_array($product, $this->products)) {
+            throw new ErrorException("Cet article est déjà dans le panier !");
+        }
+        if ($this->status !== "CART") {
+            throw new ErrorException("Votre commande n'est pas en status CART !");
+        }
+        array_push($this->products, $product);
+
+        $this->countTotalPrice();
+        $this->listProducts();
+        echo " Total : " . $this->totalPrice . "€";
+    }
+
+    public function listProducts(): void {
+        $productAsString = implode(',', $this->products);
         echo "Liste des produits : {$productAsString}";
+    }
+
+    protected function countTotalPrice(): void {
+        $this->totalPrice = count($this->products) * 5;
     }
 
     
@@ -47,4 +70,25 @@ try {
     echo $error->getMessage();
 }
 
-$order->removeProduct('Casque');
+// $order->removeProduct('Casque');
+
+try {
+    $order->addProduct('g');
+} catch(Exception $error) {
+    echo $error->getMessage();
+}
+
+
+// Choix de l'adresse de livraison
+// règles métier :
+// doit être en france, belgique ou luxembourg
+// l'adresse de livraison ne peux pas être renseignée, si la commande n'est pas en statut "CART"
+
+// Choix de la méthode de livraison
+// règles métier :
+// sélection parmi chronopost Express (+5e) , point relais et domicile
+// la méthode de livraison ne peut pas être renseignée, si l'adresse n'est pas remplie
+// Payer la commande
+// règles métier :
+// une commande ne peux pas être payée, si la méthode de livraison n'a pas eté remplie
+// Status possibles : "CART", "SHIPPING_ADDRESS_SET", "SHIPPING_METHOD_SET"  et "PAID"
