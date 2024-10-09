e8e7cb5388d97c66b8862874a3a856448274c0fc
MDorizon <mdorizon@proton.me>
2024-10-09 11:58:34 +0200

    :sparkles: header footer partials

---
+++ view/partials/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Eshop au top</title>
+	</head>
+	<body>
+
+	<header>
+		<h1>Le Eshop au top</h1>
+
+		<nav>
+			<ul>
+				<li><a href="../controller/index.php">Commander</a></li>
+				<li><a href="../controller/set-shipping-address.php">Renseigner l'adresse de livraion</a></li>
+			</ul>
+		</nav>
+	</header>
