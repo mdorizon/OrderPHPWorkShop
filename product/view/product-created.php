fd665e4a90abe3389ccb2a8fea0788ce817ef26d
MDorizon <mdorizon@proton.me>
2024-10-11 11:18:04 +0200

    :fire: removed Products ex error / success pages

--- product/view/product-created.php
+++
@@ -1,8 +0,0 @@
-<?php require_once './order/view/partials/header.php'; ?>
-	
-	<main>
-		<p>Le produit a été créée avec succès</p>
-	</main>
-
-
-<?php require_once './order/view/partials/footer.php'; ?>
